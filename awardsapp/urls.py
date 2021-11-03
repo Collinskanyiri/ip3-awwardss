cbd5065249e99609b5435f27f82abf1cf0fd10fc
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-03 12:10:44 +0300

    corections on urls

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -6,9 +6,9 @@ from awardsapp import views
 
 
 router = routers.DefaultRouter()
-router.register('users', views.ProfileViewSet)
-router.register('project', views.ProjectViewSet)
-router.register('profile', views.ReviewViewSet)
+router.register('users', views.ProfileViewset)
+router.register('project', views.ProjectViewset)
+router.register('profile', views.ReviewViewset)
 
 
 
@@ -18,10 +18,9 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('api/', include(router.urls)),
-    path('<username>/profile', views.user_profile, name='userprofile'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('profile/<username>/', views.profile, name='profile'),
-    path('profile/<username>/settings', views.edit_profile, name='edit'),
-    path('project/<post>', views.project, name='project'),
+    path('profile/<username>/update', views.update_profile, name='update_profile'),
+    path('project/<review>', views.project_review, name='project_review'),
     path('search/', views.search_project, name='search'),
 ]
