fdbc4c1ae9d62904c0712b88da085566148d5231
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-03 12:10:25 +0300

    corections on signin form

--- awardsapp/forms.py
+++ awardsapp/forms.py
@@ -2,8 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Project,Profile, Review
-from pyuploadcare.dj.forms import ImageField
-
 
 class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
@@ -24,20 +22,11 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         exclude = ['user']
 
-class UpdateUserForm(forms.ModelForm):
-    email = forms.EmailField(max_length=254, required=True)
-
-    class Meta:
-        model = User
-        fields = ('user', 'email')
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['username', 'projects', 'profile_photo', 'bio', 'contact']  
-
-class ProjectForm(forms.ModelForm):
-    project_img = ImageField(label='')
 
     class Meta:
         model = Project
@@ -46,4 +35,9 @@ class ProjectForm(forms.ModelForm):
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['Design', 'Usability', 'Content','score']        
+        fields = ['Design', 'Usability', 'Content']   
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ('project_img', 'project_name', 'project_url', 'description')
